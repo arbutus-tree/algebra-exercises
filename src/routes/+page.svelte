bc48050aafe6b66991293fbc29cf8a796d533e06
arbutus tree <daniel@debian-BULLSEYE-live-builder-AMD64>
2024-12-04 21:34:15 -0800

    basic functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  // x + a = b
+  // => x = a - b
+  let a = 0;
+  let b = 0;
+  let solution = b - a;
+  let isHidden = true;
+
+  function generateRandomInt(min,max) {
+    return Math.floor(min + Math.random()*(max-min + 1));
+  }
+
+  let newEquation = () => {
+    a = generateRandomInt(1,32);
+    b = generateRandomInt(1,32);
+    solution = b - a;
+    isHidden = true;
+  }
+
+  let showSolution = () => {
+    isHidden = false;
+  }
+</script>
+<style>
+  .hidden {
+    visibility: hidden;
+  }
+</style>
+
+
+<h1>exercises</h1>
+<button
+  on:click={newEquation}
+  >new equation</button>
+<button
+  on:click={showSolution}
+  >show solution</button>
+
+<p>x + {a} = {b}</p>
+<p class:hidden={isHidden}>x = {b} - {a}</p>
+<p class:hidden={isHidden}>x = {solution}</p>
